dde6594c8aa8972caec9be375db8bd94ebe830c8
zzMart1Nzz <b.martin.ulanga@gmail.com>
2025-03-28 09:22:13 +0100

    kontrolak sartu

---
+++ ElkarGune/src/kontrolak/KontrolAdmin.java
@@ -0,0 +1,14 @@
+package kontrolak;
+
+
+import kudeaketak.kudeaketaBazkidea;
+import objetuak.Bazkidea;
+
+public class KontrolAdmin {
+
+	public void sortuBazkidea(Bazkidea bazkidea) {
+		kudeaketaBazkidea kudeaketa = new kudeaketaBazkidea();
+		kudeaketa.sortuBazkidea(bazkidea);
+		
+	}
+}
---
+++ ElkarGune/src/kontrolak/KontrolMezuak.java
@@ -0,0 +1,13 @@
+package kontrolak;
+
+import objetuak.Mezuak;
+import kudeaketak.kudeaketaMezuak;
+
+public class KontrolMezuak {
+	
+	public void sortuMezuak(Mezuak mezuak) {
+		kudeaketaMezuak kudeaketa = new kudeaketaMezuak();
+		kudeaketa.sortuMezuak(mezuak);
+	}
+
+}
